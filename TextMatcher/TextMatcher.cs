101c67d7e0a160cf558c680f2e6dd88c2d177204
massimoahmadi <info@massimoahmadi.com>
2014-03-06 21:46:10 +0000

    Implement simple first-character matching

--- TextMatcher/TextMatcher.cs
+++ TextMatcher/TextMatcher.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace TextMatcher
 {
@@ -6,7 +6,20 @@ namespace TextMatcher
     {
 		public int[] FindMatches(string text, string subtext)
 		{
-			throw new NotImplementedException();
+			var ret = new List<int>();
+			
+			var textChars = text.ToCharArray();
+			var subtextChars = subtext.ToCharArray();
+
+			for (int i = 0; i < textChars.Length; i++)
+			{
+				if (textChars[i] == subtextChars[0])
+				{
+					ret.Add(i + 1);
+				}
+			}
+
+			return ret.ToArray();
 		}
     }
 }
