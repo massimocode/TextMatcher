2b1b23bdae1e4776596006cae1ffa70912a332c2
Massimo Hamilton <massimo.hamilton@outlook.com>
2014-03-06 21:48:05 +0000

    Add failing unit text for a 2 character subtext that should not be found

--- TextMatcher/UnitTests.cs
+++ TextMatcher/UnitTests.cs
@@ -18,5 +18,15 @@ namespace TextMatcher
 			Assert.AreEqual(1, results.Length);
 			Assert.AreEqual(1, results[0]);
 		}
+
+		[TestMethod]
+		public void When_Given_The_Main_Text_And_The_Subtext_Px_No_Matches_Should_Be_Found()
+		{
+			var matcher = new TextMatcher();
+
+			var results = matcher.FindMatches(MainText, "Px");
+
+			Assert.AreEqual(0, results.Length);
+		}
 	}
 }
