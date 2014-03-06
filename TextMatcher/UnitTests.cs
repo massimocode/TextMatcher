6e7f0aa1c61ea4f2c91672f1d7b7d76067566fa7
Massimo Ahmadi <info@massimoahmadi.com>
2014-03-06 13:26:36 -0800

    Initial commit - Create solution and add failing unit test for a simple character match. Also add Acceptance Criteria.

---
+++ TextMatcher/UnitTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TextMatcher
+{
+	[TestClass]
+	public class UnitTests
+	{
+		private const string MainText =
+			"Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea";
+
+		[TestMethod]
+		public void When_Given_The_Main_Text_And_The_Letter_P_The_Result_Should_Be_1()
+		{
+			var matcher = new TextMatcher();
+
+			var results = matcher.FindMatches(MainText, "P");
+
+			Assert.AreEqual(1, results.Length);
+			Assert.AreEqual(1, results[0]);
+		}
+	}
+}
