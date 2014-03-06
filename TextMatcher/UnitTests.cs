1229e08b6c58a9393719f307878614c0d161cf05
Massimo Hamilton <massimo.hamilton@outlook.com>
2014-03-06 22:05:18 +0000

    Extra Development - Added failing unit test to highlight exception thrown when empty subtext string is provided

--- TextMatcher/UnitTests.cs
+++ TextMatcher/UnitTests.cs
@@ -7,5 +7,15 @@ namespace TextMatcher
 	{
 		private const string MainText =
 			"Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea";
+
+		[TestMethod]
+		public void When_Given_The_Main_Text_And_An_Empty_String_There_Should_Be_No_Matches()
+		{
+			var matcher = new TextMatcher();
+
+			var results = matcher.FindMatches(MainText, "");
+
+			Assert.AreEqual(0, results.Length);
+		}
 	}
 }
